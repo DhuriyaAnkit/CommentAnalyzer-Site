27ffbacf02ef5efd6d54b21b9365ed259c9c495c
Dhuriya Ankit Subhash <ankitdhuriya281@gmail.com>
2025-08-31 21:58:03 +0530

    Fix build issues for Render deployment

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+# Use a specific Python version to install packages
+python3.9 -m pip install -r requirements.txt
+# Use the same Python version to run Django commands
+python3.9 manage.py collectstatic --noinput 
+python3.9 manage.py migrate
